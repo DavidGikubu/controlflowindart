c380f0d0fc8742f970114349c50e26a42a440ba9
Gikubu <davidndungu8203@gmail.com>
2024-03-06 20:03:20 +0300

    this loop counts from 10 -1.

--- forloop.dart
+++ forloop.dart
@@ -1,7 +1,12 @@
 void main() {
   //print 1-10
-  int i = 1;
-  for (i = 1; i <= 10; i++) {
+
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+  print(" ");
+  //printing 10-1
+  for (int i = 10; i >= 1; i--) {
     print(i);
   }
 }
